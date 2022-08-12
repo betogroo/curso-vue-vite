aafe03405f5e3c176ffd27aeae7a7ce30676f6d0
betogroo <luizhumberto@gmail.com>
2022-08-12 11:29:37 -0300

    fix: add v-app, v-main and v-container in App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <v-app>
     <div class="text-center">
       <router-link :to="{ name: 'HomeView' }">Home</router-link> |
       <router-link :to="{ name: 'AboutView' }">About</router-link>
     </div>
-    <router-view />
-  </div>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts"></script>
