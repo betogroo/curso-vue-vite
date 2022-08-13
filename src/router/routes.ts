7c8bad32812f319b669f9d755b3e9463b70d662c
Luiz Humberto <luizhumberto@gmail.com>
2022-08-13 16:33:16 -0300

    feat: create CounterView route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import { RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import NotFound from '../views/NotFound.vue'
+import CounterView from '../views/CounterView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/about',
     name: 'AboutView',
     component: AboutView
+  },
+  {
+    path: '/counter',
+    name: 'CounterView',
+    component: CounterView
   },
   {
     path: '/:catchAll(.*)',
---
+++ src/views/CounterView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Contador</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
