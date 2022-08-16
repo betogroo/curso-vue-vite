fac4f27ebd188d9e6e77ba3971a15e76741e299b
betogroo <luizhumberto@gmail.com>
2022-08-16 14:10:24 -0300

    refactor: build drawer with looping v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
-<template>
-  <div>
-    <div class="text-center">
-      <router-link :to="{ name: 'HomeView' }">Home</router-link> |
-      <router-link :to="{ name: 'AboutView' }">About</router-link>
-    </div>
-    <router-view />
-  </div>
-</template>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<script setup lang="ts"></script>
+interface DrawerItems {
+  name: string
+  title: string
+  icon: string
+}
+
+const items = ref<DrawerItems[]>([
+  {
+    name: 'HomeView',
+    icon: 'mdi-home',
+    title: 'Home'
+  },
+  {
+    name: 'CounterView',
+    icon: 'mdi-counter',
+    title: 'Counter'
+  },
+  {
+    name: 'AboutView',
+    icon: 'mdi-information',
+    title: 'About'
+  }
+])
+</script>
+<template>
+  <v-app>
+    <v-navigation-drawer permanent>
+      <v-list nav density="compact">
+        <v-list-item
+          v-for="item in items"
+          :key="item.name"
+          :prepend-icon="item.icon"
+          :title="item.title"
+          :to="{ name: item.name }"
+        />
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
 
 <style scoped></style>
