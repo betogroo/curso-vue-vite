dd26eee3ad983230d4fc8cb24dd5949cc9343034
betogroo <luizhumberto@gmail.com>
2022-08-17 12:57:15 -0300

    feat: add cards to AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>About Page</div>
+  <div>
+    <v-card>
+      <v-card-title>Autor</v-card-title>
+      <v-card-item
+        subtitle="Brasil"
+        title="Robert"
+        prepend-icon="mdi-account"
+      ></v-card-item>
+      <v-card-item title="Robert" prepend-icon="mdi-information"></v-card-item>
+    </v-card>
+    <v-card>
+      <v-card-title>Empresa</v-card-title>
+      <v-card-item
+        subtitle="Especialistas em Javascript"
+        title="JS Programers "
+        prepend-icon="mdi-domain"
+      ></v-card-item>
+      <v-card-item
+        subtitle="Av Brigadeiro Faria Lima, 245 - São Paulo SP "
+        prepend-icon="mdi-map-marker"
+      ></v-card-item>
+    </v-card>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
